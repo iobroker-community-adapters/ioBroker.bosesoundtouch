e64051046c8152597c302f7f989d571a9dd9cb63
Harald <harald.wick@gmx.at>
2017-11-01 17:22:41 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,288 @@
+/**
+ *
+
+
+/* jshint -W097 */// jshint strict:false
+/*jslint node: true */
+"use strict";
+
+const BOSE_ID_CONTROL = '';//'control.';
+    const BOSE_ID_ON    = BOSE_ID_CONTROL + 'on';
+    const BOSE_ID_KEY    = BOSE_ID_CONTROL + 'key';
+    const BOSE_ID_VOLUME = BOSE_ID_CONTROL + 'volume';
+    const BOSE_ID_MUTED  = BOSE_ID_CONTROL + 'muted';
+
+const BOSE_ID_INFO = '';//'info.';
+const BOSE_ID_NOW_PLAYING = BOSE_ID_INFO + 'nowPlaying.';
+    const BOSE_ID_NOW_PLAYING_SOURCE  = BOSE_ID_NOW_PLAYING + 'source';
+    const BOSE_ID_NOW_PLAYING_TRACK   = BOSE_ID_NOW_PLAYING + 'track';
+    const BOSE_ID_NOW_PLAYING_ARTIST  = BOSE_ID_NOW_PLAYING + 'artist';
+    const BOSE_ID_NOW_PLAYING_ALBUM   = BOSE_ID_NOW_PLAYING + 'album';
+    const BOSE_ID_NOW_PLAYING_STATION = BOSE_ID_NOW_PLAYING + 'station';
+
+const BOSE_ID_PRESETS = BOSE_ID_INFO + 'presets.';
+    const BOSE_ID_PRESET_SOURCE = BOSE_ID_PRESETS + '{}.source';
+    const BOSE_ID_PRESET_NAME   = BOSE_ID_PRESETS + '{}.name';
+    const BOSE_ID_PRESET_ICON   = BOSE_ID_PRESETS + '{}.iconUrl';
+
+const BOSE_ID_DEVICE_INFO = BOSE_ID_INFO + 'deviceInfo.';
+    const BOSE_ID_INFO_NAME = BOSE_ID_DEVICE_INFO + 'name';
+    const BOSE_ID_INFO_TYPE = BOSE_ID_DEVICE_INFO + 'type';
+    const BOSE_ID_INFO_MAC_ADDRESS = BOSE_ID_DEVICE_INFO + 'macAddress';
+    
+
+// you have to require the utils module and call adapter function
+var format = require('string-format');
+var utils = require(__dirname + '/lib/utils'); // Get common adapter utils
+var soundtouchsocket = require(__dirname + '/soundtouchsocket');
+//var bosestates = require(__dirname + '/bosestates');
+
+// you have to call the adapter function and pass a options object
+// name has to be set and has to be equal to adapters folder name and main file name excluding extension
+// adapter will be restarted automatically every time as the configuration changed, e.g system.adapter.template.0
+class boseAdapter {
+    constructor() {
+        var instance = this;
+        
+        this.adapter = utils.adapter('bosesoundtouch');
+
+        // is called when adapter shuts down - callback has to be called under any circumstances!
+        this.adapter.on('unload', function(callback) { instance.onUnload(callback); });
+
+        // is called if a subscribed object changes
+        this.adapter.on('objectChange', function(id, obj) { instance.onObjectChange(id, obj); });
+
+        // is called if a subscribed state changes
+        this.adapter.on('stateChange', function (id, state) { instance.onStateChange(id, state); });
+        
+        // Some message was sent to adapter instance over message box. Used by email, pushover, text2speech, ...
+        this.adapter.on('message', function(obj) { instance.onMessage(obj); });
+
+        // is called when databases are connected and adapter received configuration.
+        // start here!
+        this.adapter.on('ready', function() { instance.onReady(); });
+    }
+
+    onUnload(callback) {
+        try {
+            this.adapter.log.info('cleaned everything up...');
+            callback();
+        } 
+        catch (e) {
+            callback();
+        }
+    }
+
+    onObjectChange(id, obj) {
+        // Warning, obj can be null if it was deleted
+        this.adapter.log.info('objectChange ' + id + ' ' + JSON.stringify(obj));
+    }
+
+    onStateChange(id, state) {
+        // Warning, state can be null if it was deleted
+        this.adapter.log.info('stateChange ' + id + ' ' + JSON.stringify(state));
+        
+        // you can use the ack flag to detect if it is status (true) or command (false)
+        if (state && !state.ack) {
+            switch (id) {
+                case 'bosesoundtouch.0.' + BOSE_ID_ON:
+                    this.adapter.setState(BOSE_ID_KEY, 'POWER');
+                    break;
+        
+                case 'bosesoundtouch.0.' + BOSE_ID_VOLUME:
+                    this.socket.setValue('volume', '', state.val);
+                    break;
+                
+                case 'bosesoundtouch.0.' + BOSE_ID_KEY:
+                    this.socket.setValue('key', 'state="press" sender="Gabbo"', state.val);
+                    this.socket.setValue('key', 'state="release" sender="Gabbo"', state.val);
+                    break;
+
+                case 'bosesoundtouch.0.' + BOSE_ID_MUTED:
+                    this.adapter.setState(BOSE_ID_KEY, 'MUTE');
+                    break;
+            }
+            this.adapter.log.info('ack is not set!');
+        }
+    }
+
+    onMessage(obj) {
+        if (typeof obj == 'object' && obj.message) {
+            if (obj.command == 'send') {
+                // e.g. send email or pushover or whatever
+                console.log('send command');
+
+                // Send response in callback if required
+                if (obj.callback) {
+                    this.adapter.sendTo(obj.from, obj.command, 'Message received', obj.callback);
+                }
+            }
+        }
+    }
+
+    onReady() {        
+        this.initObjects();
+        this.adapter.subscribeStates('*');
+        
+        // in this template all states changes inside the adapters namespace are subscribed
+        this.socket = new soundtouchsocket(this.adapter.config.address);
+        
+        var instance = this;
+
+        this.socket.connect().then( function(server) { 
+            instance.socket.addListener('deviceInfo', function(obj) { instance.setDeviceInfo(obj); });
+            instance.socket.addListener('volume', function(obj) { instance.setVolume(obj); });
+            instance.socket.addListener('nowPlaying', function(obj) { instance.setNowPlaying(obj); });
+            instance.socket.addListener('presets', function(obj) { instance.setPresets(obj); });
+            instance.socket.updateAll(server); 
+        }).catch(function(err) {
+            // error here
+            instance.adapter.error(err);
+        });    
+    }
+
+    initObjects() {
+        this.adapter.setObject(BOSE_ID_KEY, {
+            type: 'state',
+            common: {
+                name: 'key',
+                type: 'string',
+                role: 'text',
+                read: false,
+                write: true
+            },
+            native: {}
+        });        
+
+        this.adapter.setObject(BOSE_ID_VOLUME, {
+            type: 'state',
+            common: {
+                name: 'volume',
+                type: 'number',
+                role: 'level',                
+                read: true,
+                write: true
+
+            },
+            native: {}
+        });
+        
+        this.adapter.setObject(BOSE_ID_MUTED, {
+            type: 'state',
+            common: {
+                name: 'muted',
+                type: 'boolean',
+                role: 'indicator',                
+                read: true,
+                write: true
+            },
+            native: {}
+        });
+        
+        this.adapter.setObject(BOSE_ID_ON, {
+            type: 'state',
+            common: {
+                name: 'on',
+                type: 'boolean',
+                role: 'indicator',                
+                read: true,
+                write: true
+            },
+            native: {}
+        });
+        
+        const presetsConfig = {
+            type: 'state',
+            common: {
+                name: '',
+                type: 'string',
+                read: true,
+                write: false
+            },
+            native: {}
+        };
+
+        for (var i = 1; i <= 6; i++) {
+            presetsConfig.common.role = 'text';
+            this.adapter.setObject(format(BOSE_ID_PRESET_SOURCE, i), presetsConfig);
+            this.adapter.setObject(format(BOSE_ID_PRESET_NAME, i),   presetsConfig);
+            presetsConfig.common.role = 'text.url'
+            this.adapter.setObject(format(BOSE_ID_PRESET_ICON, i),   presetsConfig);
+        }
+
+        const nowPlayingConfig = {
+            type: 'state',
+            common: {
+                name: '',
+                type: 'string',
+                role: 'text',                
+                read: true,
+                write: false
+            },
+            native: {}
+        };
+        this.adapter.setObject(BOSE_ID_NOW_PLAYING_SOURCE,  nowPlayingConfig);
+        this.adapter.setObject(BOSE_ID_NOW_PLAYING_TRACK,   nowPlayingConfig);
+        this.adapter.setObject(BOSE_ID_NOW_PLAYING_ARTIST,  nowPlayingConfig);
+        this.adapter.setObject(BOSE_ID_NOW_PLAYING_ALBUM,   nowPlayingConfig);
+        this.adapter.setObject(BOSE_ID_NOW_PLAYING_STATION, nowPlayingConfig);
+
+        const deviceInfoConfig = {
+            type: 'state',
+            common: {
+                name: '',
+                type: 'string',
+                role: 'text',
+                read: true,
+                write: false
+            },
+            native: {}
+        };
+        this.adapter.setObject(BOSE_ID_INFO_NAME, deviceInfoConfig);
+        this.adapter.setObject(BOSE_ID_INFO_TYPE, deviceInfoConfig);
+        this.adapter.setObject(BOSE_ID_INFO_MAC_ADDRESS, deviceInfoConfig);
+    }
+
+    setDeviceInfo(obj) {
+        this.adapter.setState(BOSE_ID_INFO_NAME, {val: obj.name, ack: true})
+        this.adapter.setState(BOSE_ID_INFO_TYPE, {val: obj.type, ack: true})
+        this.adapter.setState(BOSE_ID_INFO_MAC_ADDRESS, {val: obj.macAddress, ack: true})
+    }
+
+    setVolume(obj) {
+        var actualVolume = obj.actualvolume;
+        var muted = (obj.muteenabled == "true");
+        this.adapter.setState(BOSE_ID_VOLUME, {val: actualVolume, ack: true});
+        this.adapter.setState(BOSE_ID_MUTED, {val: muted, ack: true});
+    }
+
+    setNowPlaying(obj) {
+        this.adapter.setState(BOSE_ID_NOW_PLAYING_SOURCE, {val: obj.source, ack: true});
+    	this.adapter.setState(BOSE_ID_NOW_PLAYING_TRACK, {val: obj.track, ack: true});
+    	this.adapter.setState(BOSE_ID_NOW_PLAYING_ARTIST, {val: obj.artist, ack: true});
+    	this.adapter.setState(BOSE_ID_NOW_PLAYING_ALBUM, {val: obj.album, ack: true});
+        this.adapter.setState(BOSE_ID_NOW_PLAYING_STATION, {val: obj.station, ack: true});
+        
+        this.adapter.setState(BOSE_ID_ON, {val: (obj.source != 'STANDBY'), ack: true});
+    }
+
+    setPresets(obj) {
+        //var presets = JSON.parse(data);
+        for (var i = 0; i < 6; i++) {            
+            var preset = {
+				source: 	'',
+				name:		'',
+                iconUrl:	''
+            };
+            if (obj[i]) {
+                preset = obj[i];
+            }
+
+            this.adapter.setState(format(BOSE_ID_PRESET_SOURCE, i+1), {val: preset.source, ack: true});
+            this.adapter.setState(format(BOSE_ID_PRESET_NAME, i+1), {val: preset.name, ack: true});
+            this.adapter.setState(format(BOSE_ID_PRESET_ICON, i+1), {val: preset.iconUrl, ack: true});
+        }
+    }
+}
+
+var adapter = new(boseAdapter);
---
+++ soundtouchsocket.js
@@ -0,0 +1,321 @@
+'use strict';
+
+module.exports = class soundtouchsocket extends require('events').EventEmitter {
+
+    constructor(address) {
+        super();
+    	if (!address) {
+    		throw new Error('soundtouchsocket needs an address');
+    	}
+    	this.address = address;
+    	this.request = require('request');
+    	this.promise = require('es6-promise');
+    	this.xml2js = require('xml2js');
+    }
+    
+    connect() {
+    	console.debug('connect');
+    	var instance = this;
+
+    	return new this.promise.Promise(function(resolve, reject) {
+	        var address = 'ws://' + instance.address + ':8080/';
+	        const WebSocket = require('ws');
+	        
+		    console.log('connecting to host ' + address);
+		    instance.ws = new WebSocket(address, "gabbo");
+		    
+		    instance.ws.on('open', function() { instance._onOpen(resolve); });
+		    instance.ws.on('close', function() { instance._onClose(); });
+    		instance.ws.on('error', function(error) { instance._onError(error, reject); });
+    		instance.ws.on('message', function(data, flags) { instance._onMessage(data, flags); });
+    	});
+    }
+    
+	_heartBeatFunc() {
+	    console.debug('_heartBeatFunc');
+		if (Date.now() - this._lastMessage > 30000) {
+			console.warn('heartbeat timeout');
+			this.ws.close();
+			clearInterval(this.heartBeatInterval);
+		}
+		else {
+    		//console.log('<span style="color:darkblue;">sending heartbeat');
+    		this.send('webserver/pingRequest');
+		}
+    }
+    
+    _restartHeartBeat() {
+        console.debug('_restartHeartBeat');
+    	this._lastMessage = Date.now();
+        clearInterval(this.heartBeatInterval);
+        var instance = this;
+        this.heartBeatInterval = setInterval(function() { instance._heartBeatFunc() }, 10000);
+    }
+    
+    _onOpen(resolve) {
+        console.debug('onOpen');
+        var instance = this;
+        this._restartHeartBeat();
+		this.emit('connected');
+		resolve();
+	}
+	
+	_onClose() {
+        console.log('onClose');
+		clearInterval(this.heartBeatInterval);
+		this.emit('closed');
+	}
+		
+    _onError(error, reject) {
+        console.error('websocket error ' + error);
+	    this.rmit('error', error);
+		reject();
+    }
+    
+    _onMessage(data, flags) {
+        console.debug('onMessage');
+        this._parse(data);
+    }
+    
+    send(data) {
+    	var instance = this;
+    	return new this.promise.Promise(function(resolve, reject) {
+    	    console.log('Send: ' + data);
+    		instance.ws.send(data, function ackSend(err) {
+    			if (err) {
+    				reject(err);
+    			} 
+    			else {
+    				resolve();
+    			}
+    		});
+    	});        
+    }
+    
+    _handleVolume(volume) {
+    	console.log('received [volume]:' + volume.actualvolume);
+    	//this.emit('volume', JSON.stringify(volume));
+    	this.emit('volume', volume);
+	}
+	
+	_handlePresets(data) {
+		//console.log('for:' + JSON.stringify(data.presets));
+		var preset = data.presets.preset;
+		var object = [];
+	    for (var i in preset) {
+			var contentItem = preset[i].ContentItem;
+			object[preset[i].id - 1] = {
+				source: 	contentItem.source,
+				name:		contentItem.itemName,
+				iconUrl:	contentItem.containerArt
+			};
+		}
+		this.emit('presets', object);
+	}
+
+	_handleDeviceInfo(data) {
+		console.log('received [info] ' + JSON.stringify(data));
+		var object = {};/*
+			name: '',
+			type: '',
+			swVersion: '',
+			macAddress: '',
+			ipAddress: ''
+		};*/
+
+		object.name = data.name;
+		object.type = data.type;
+		object.macAddress = data.deviceID;
+		this.emit('deviceInfo', object);
+	}
+    
+    _handleNowPlaying(data) {
+        console.log('received [now_playing] ' + JSON.stringify(data));
+        var source = data.source;
+        var track = '';
+        var artist = '';
+        var album = '';
+        var station = '';
+        switch (source) {
+            case 'INTERNET_RADIO':
+            case 'BLUETOOTH':
+                track = data.track;
+                artist = data.artist;
+                album = data.album;
+                station = data.stationName;
+                break;
+                
+            case 'PRODUCT':
+                station = data.sourceAccount;
+                break;
+        }
+        var object = {
+            source:     source,
+    	    track:      track,
+    	    artist:     artist,
+    	    album:      album,
+    	    station:    station
+        };
+    	this.emit('nowPlaying', object);
+    }
+
+    _onJsData(jsData) {
+        console.log(JSON.stringify(jsData));
+		for (var infoItem in jsData) {
+			switch(infoItem) {
+				case 'info':
+					this._handleDeviceInfo(jsData[infoItem]);
+					break;
+					
+				case 'nowPlaying':
+					this._handleNowPlaying(jsData[infoItem]);
+					break;
+					
+				case 'bass':
+					this._handleBassInfo(jsData[infoItem]);
+					break;
+					
+				case 'bassCapabilities':
+					this._handleBassCaps(jsData[infoItem]);
+					break;
+					
+				case 'volume': {
+				    var volume = jsData.volume;
+				    if (volume) {
+				        this._handleVolume(volume);
+				    }
+					break;
+				}
+				
+				case 'presets':
+					this._handlePresets(jsData);
+					break;
+					
+				case 'sources':
+					this._handleSources(jsData[infoItem]);
+					break;
+					
+				case 'zone':
+					this._handleZone(jsData[infoItem]);
+					break;
+					
+				case 'trackInfo':
+					this._handleTrackInfo(jsData[infoItem]);
+					break;   
+					
+				case 'updates':
+				    if (jsData.hasOwnProperty('updates')) {
+						for (var updateItem in jsData.updates) {
+							switch(updateItem) {
+							    case 'nowPlayingUpdated': {
+                                    var nowPlaying = jsData.updates.nowPlayingUpdated.nowPlaying;
+                                	if (nowPlaying) {
+                                		this._handleNowPlaying(nowPlaying);
+                                	} 
+                                	else {
+                                		this.getInfo();
+                                	}
+                                	break;
+                                }
+                                
+                                case 'volumeUpdated': {
+                                    var volume = jsData.updates.volumeUpdated.volume;
+                                  	if (volume) {
+                                        this._handleVolume(volume);
+                                  	}
+                                	else {
+                                		this.getVolume();
+                                	}
+                                	break;
+                                }
+
+							}
+						}
+				    }
+				    break;
+			}
+        }
+        
+        this._restartHeartBeat();
+    }
+    
+    _parse(xml) {
+        var instance = this;
+        this.xml2js.parseString(xml, {explicitArray: false, mergeAttrs: true}, function(err, jsData) { 
+            if (err) {
+                console.error(err);
+            }
+            else {
+                instance._onJsData(jsData);
+            }
+        });        
+    }
+    
+    setValue(command, args, value) {
+    	if (args !== '' && args[0] != ' ') {
+            args = ' ' + args;
+        }
+        var bodyString = '<' + command + args + '>' + value + '</' + command + '>';
+        var urlString = 'http://' + this.address + ':8090/';
+        this.request.post( {
+                baseUrl:    urlString,
+                uri:        command,
+                body:       bodyString
+            }, 
+            function(error, response, body) {
+                //log(' ----  response: ' + response.statusCode + ' --- body: ' + body, 'info');
+                if (error) {
+                    console.error(error, 'error');
+                }
+            }
+        );
+        console.log('setValue: ' + urlString + command + ' - ' + bodyString);
+    }
+    
+    get(value) {
+        var instance = this;
+        var command = 'http://' + this.address + ':8090/' + value;
+        console.log('request: ' + command);
+        this.request.get(command, function(error, response, body) {  
+            if (error) {
+                console.error(response.statusCode);
+            }
+            else {
+                instance._parse(body);
+            }
+        });
+	}
+	
+	getDeviceInfo() {
+		this.get('info');
+	}
+    
+    getPlayInfo() {
+        this.get('now_playing');
+    }
+    
+    getPresets() {
+        this.get('presets');
+    }
+    
+    getVolume() {
+        this.get('volume');
+    }
+    
+    updateAll() {
+        console.debug('updateAll');
+    	var instance = this;
+    	return this.promise.Promise.all([
+    		instance.getDeviceInfo(),
+    		instance.getPlayInfo(),
+    		instance.getPresets(),
+    		//_instance.getBassCapabilities(),
+    		//_instance.getBassInfo(),
+    		instance.getVolume(),
+    		//_instance.getSources(),
+    		//_instance.getZone(),
+    		//_instance.getTrackInfo()
+    		]);
+    }
+    
+}
