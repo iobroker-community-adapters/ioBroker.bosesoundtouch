a246142b897b2f27f51408a4783503b4222770eb
Ingo Fischer <github@fischer-ka.de>
2017-11-20 11:00:50 +0100

    update, add testing and all

--- admin/words.js
+++ admin/words.js
@@ -1,6 +1,6 @@
 // Dictionary (systemDictionary is global variable from adapter-settings.js)
 systemDictionary = {
-	"template adapter settings": {"en": "Example", "de": "Beispiel"},
+	"Bose Soundtouch adapter settings": {"en": "Bose Soundtouch adapter settings", "de": "Bose Soundtouch adapter Einstellungen"},
 	"address":                   {"en": "IP address", "de": "IP-Adresse"},
 	"on save adapter restarts with new config immediately": {
 		"en": "Once the settings are saved, the adapter will be restarted immediately.",
